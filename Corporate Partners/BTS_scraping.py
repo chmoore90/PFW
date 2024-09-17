4e2741b462cdbb157225abc8ea30f2ceb4a2ed91
chmoore <chmoore90@gmail.com>
2024-09-17 18:25:23 -0400

    functionaly complete

--- Corporate Partners/BTS_scraping.py
+++ Corporate Partners/BTS_scraping.py
@@ -1,10 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+from zipfile import ZipFile
+from io import BytesIO
 import pandas as pd
 
-files = []
+# preparing soup
+site_url = "https://www.bts.gov/browse-statistical-products-and-data/bts-publications/data-bank-28ds-t-100-domestic-segment-data"
+site = requests.get(site_url)
+soup = BeautifulSoup(site.text, "lxml")
+page = soup.find_all("a", href=True)
 
-# reading file and renaming relevant columns
-data = pd.read_csv("C:\\Users\\chmoo\\Desktop\\db28seg.dd.wac.202307.202406.asc", sep="|", header=None)
-data.rename(columns={6: "origin", 10: "destin", 11: "code"}, inplace=True)
+# list of files
+dfs = []
 
+# creating list of dataframes from website zip files
+for i in page:
+    date = i.get_text()
+    # LIMITING: to dates 2024
+    if date[-4:] == "2024":
+        link_url = i["href"]
+        if link_url.startswith("/sites/bts.dot.gov/files/docs/airline-data/domestic-segments/"):
+            parameters = {"downloadformat" : "csv"}
+            response = requests.get(f"https://www.bts.gov{link_url}", parameters)
 
-print(data.head)
+            # unzipping zip file, appending dataframe to dfs list
+            z = ZipFile(BytesIO(response.content))
+            df = pd.read_csv(z.open(z.namelist()[-1]), sep="|", header=None)
+            df.rename(columns={5: "origin", 9: "destin", 10: "code"}, inplace=True)
+            dfs.append(df)
+
+# compiling datasets into one
+data = pd.concat(dfs)[["origin", "destin", "code"]]
+
+print(data.head(50))
